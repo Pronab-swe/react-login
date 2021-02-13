c52c31f9ec5f4e6feabe6a8d6bbefdb801c290be
Pronab21 <bpronab21@gmail.com>
2021-02-13 18:15:11 +0600

    Updated Login

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./App.js";
 import './App.scss';
- //import HomePage from "./Pages/Home/Index";
+ import HomePage from "./Pages/Home/Index";
 //import AboutPage from './Pages/About/Index';
 //import AboutTry from "./Try/About/try/Index";
-import DashboardPage from "./Pages/Home/Dashboard/Index";
+//import DashboardPage from "./Pages/Home/Dashboard/Index";
 
 
 function App(){
   return (
     <div className='App'>
-      {/* <HomePage/> */}
+      <HomePage/>
       {/* <AboutPage /> */}
       {/* <AboutTry/> */}
-      <DashboardPage/>
+      {/* <DashboardPage/> */}
     </div>
   )
 }
